5e372263a89ea710cce98172ab155ae75414e294
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-13 01:32:40 -0300

    Update mailaccess.py

--- mailaccess.py
+++ mailaccess.py
@@ -1,7 +1,7 @@
 import requests
 email=input('email: ')
-pass=input('pass: ')
-mail=requests.get('https://aj-https.my.com/cgi-bin/auth?model=&simple=1&Login='+email+'&Password='+pass, headers={'User-Agent': 'MyCom/12436 CFNetwork/758.2.8 Darwin/15.0.0'}).text
+Pass=input('pass: ')
+mail=requests.get('https://aj-https.my.com/cgi-bin/auth?model=&simple=1&Login='+email+'&Password='+Pass, headers={'User-Agent': 'MyCom/12436 CFNetwork/758.2.8 Darwin/15.0.0'}).text
 if ("Ok=0") in mail:
 	print('failed')
 else:
